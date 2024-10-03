3a6bb94c161bcde7e28def64ad2a39067a991d33
Shen Jiamin <shen_jiamin@u.nus.edu>
2024-10-03 19:00:52 +0800

    api: add `quiz_submission_attempt` param
    Signed-off-by: Shen Jiamin <shen_jiamin@u.nus.edu>

--- src/nice_canvas/api/http.py
+++ src/nice_canvas/api/http.py
@@ -80,13 +80,16 @@ class QuizSubmissionQuestionsApi:
     def __init__(self, canvas: CanvasClient) -> None:
         self.canvas = canvas
 
-    def index(self, quiz_submission_id):
+    def index(self, quiz_submission_id, attempt=None):
         """
         Get all quiz submission questions.
         https://canvas.instructure.com/doc/api/quiz_submission_questions.html
         """
         url = f"/api/v1/quiz_submissions/{quiz_submission_id}/questions"
-        return self.canvas.get(url).json().get("quiz_submission_questions")
+        params = {"quiz_submission_attempt": attempt} if attempt else {}
+        return (
+            self.canvas.get(url, params=params).json().get("quiz_submission_questions")
+        )
 
 
 class SubmissionsApi:
