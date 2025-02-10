5f75c03182e2c264d744a58ad858287bb8c49fda
Shen Jiamin <shen_jiamin@u.nus.edu>
2025-02-10 13:09:44 +0800

    remove duplicated create_question_bank
    Signed-off-by: Shen Jiamin <shen_jiamin@u.nus.edu>

--- src/nice_canvas/qti/question.py
+++ src/nice_canvas/qti/question.py
@@ -156,10 +156,3 @@ class ShortAnswerQuestion(EssayQuestion):
                 continue_="No",
             ),
         )
-
-
-def create_question_bank(questions: Iterable[Question]):
-    root = binding.questestinterop()
-    for question in questions:
-        root.append(question.xml)
-    return root
