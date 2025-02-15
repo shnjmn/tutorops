8574a927f8163dd67ae070dd292598cd2ee185f9
Shen Jiamin <shen_jiamin@u.nus.edu>
2025-02-15 13:25:15 +0800

    allow bank title for question banks

--- src/nice_canvas/qti/interop.py
+++ src/nice_canvas/qti/interop.py
@@ -1,11 +1,23 @@
 from typing import Iterable
+from uuid import uuid1
 
 from . import binding
 from .question import Question
 
 
-def create_question_bank(questions: Iterable[Question]):
-    root = binding.questestinterop()
+def create_question_bank(bank_title, questions: Iterable[Question]):
+    bank = binding.objectbank(ident=str(uuid1()))
+
+    bank.append(
+        binding.qtimetadata(
+            binding.qtimetadatafield(
+                fieldlabel="bank_title",
+                fieldentry=bank_title,
+            ),
+        )
+    )
+
     for question in questions:
-        root.append(question.xml)
-    return root
+        bank.append(question.xml)
+
+    return binding.questestinterop(bank)
