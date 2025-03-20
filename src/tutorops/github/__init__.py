7d16350e0e0abb6ff38e47a6e5e47f5e09ad9cbe
Shen Jiamin <shen_jiamin@u.nus.edu>
2025-03-20 12:34:30 +0800

    github: read token from environ

--- src/tutorops/github/__init__.py
+++ src/tutorops/github/__init__.py
@@ -1,8 +1,16 @@
+import os
+
 import httpx
 
 
 class GitHub:
-    def __init__(self, token: str):
+    def __init__(self, token: str = None):
+        if not token:
+            token = os.getenv("GITHUB_TOKEN")
+
+        if not token:
+            raise ValueError("GitHub token is required")
+
         self.http = httpx.Client(
             base_url="https://api.github.com",
             headers={
@@ -15,6 +23,9 @@ class GitHub:
         )
 
     def list_accepted_assignments(self, assignment_id: int, per_page: int = 100):
+        """
+        https://docs.github.com/en/rest/classroom/classroom?apiVersion=2022-11-28#list-accepted-assignments-for-an-assignment
+        """
         resp = self.http.get(
             f"/assignments/{assignment_id}/accepted_assignments",
             params={"per_page": per_page},
